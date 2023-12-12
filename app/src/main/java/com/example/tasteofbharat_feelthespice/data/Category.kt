4981a976fc1f440dfa7dfa8123fa78afb3fc9c8a
panch <w9620369@live.tees.ac.uk>
2023-12-12 19:57:39 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/deploymentTargetDropDown.xml
    #	app/build.gradle.kts
    #	app/src/main/java/com/example/tasteofbharat_feelthespice/app/TasteOfBharat.kt
    #	app/src/main/java/com/example/tasteofbharat_feelthespice/data/signUp/Validator.kt
    #	app/src/main/java/com/example/tasteofbharat_feelthespice/navigation/TasteOfBharatRouter.kt
    #	app/src/main/java/com/example/tasteofbharat_feelthespice/screens/HomeScreen.kt
    #	app/src/main/java/com/example/tasteofbharat_feelthespice/screens/TOBSplashScreen.kt
    #	app/src/main/java/com/example/tasteofbharat_feelthespice/viewmodels/HomeViewModel.kt
    #	app/src/main/res/layout/activity_insert_data.xml

--- app/src/main/java/com/example/tasteofbharat_feelthespice/data/Category.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tasteofbharat_feelthespice.data
-
-data class Category(
-    val title: String
-)
--- app/src/main/java/com/example/tasteofbharat_feelthespice/data/login/LoginUIState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tasteofbharat_feelthespice.data.login
-
-data class LoginUIState(
-    var email  :String = "",
-    var password  :String = "",
-
-    var emailError :Boolean = false,
-    var passwordError : Boolean = false
-)
